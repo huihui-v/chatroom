e4333e05cb5e4229b212bbee0678e90f9f81fb32
Jianfei2333 <xjf999999@hotmail.com>
2017-05-16 16:00:39 +0800

    v2.1,实现聊天功能，用户列表获取功能，登录消息推送功能

--- client/client.py
+++ client/client.py
@@ -6,7 +6,7 @@ f = open("config.json", 'r')
 setting = json.load(f)
 host = setting['host']
 port = int(setting['port'])
-info = {"status": "", "info": "", "targetip": ""}
+info = {"status": "", "info": "", "targetip": "", "body": ""}
 
 def create_socket():
     try:
@@ -28,7 +28,7 @@ def connect_to_server():
     return s;
 
 def input_command ():
-    string = raw_input("input your command: ")
+    string = raw_input()
     return str(string).split(' ');
 
 def disconnect(s):
@@ -36,11 +36,6 @@ def disconnect(s):
     new_info['status'] = "DISCONNECT"
     in_json = json.dumps(new_info);
     s.sendall(in_json)
-    recv_info = s.recv(1024)
-    recv_info = json.loads(recv_info);
-    if recv_info['status'] == 'OK':
-        s.close()
-    print 'Disconnected!'
 
 def send_msg(target, msg, s):
     new_info = info;
@@ -49,27 +44,46 @@ def send_msg(target, msg, s):
     new_info['info'] = msg
     in_json = json.dumps(new_info);
     s.sendall(in_json)
-    recv_info = s.recv(1024)
-    recv_info = json.loads(recv_info);
-    if recv_info['status'] == 'OK':
-        print 'Message sent!'
-    elif recv_info['status'] == 'OFFLINE':
-        print 'Error: Receiver Offline!'
-    return;
+
+def send_request(s, req):
+    new_info = info;
+    new_info['status'] = "GET"
+    new_info['body'] = req
+    in_json = json.dumps(new_info)
+    s.sendall(in_json)
 
 def listen (s):
     while 1:
         recv_info = s.recv(1024)
         recv_info = json.loads(recv_info);
-        if recv_info['status'] == 'SEND':
+        if recv_info['status'] == 'RECV':
             print 'Receive from ', recv_info['sourceip'], ':'
             print recv_info['info']
             continue;
-
+        elif recv_info['status'] == 'RESP':
+            for i in range(0, len(recv_info['body'])):
+                print recv_info['body'][i][0]+":"+str(recv_info['body'][i][1])
+            continue;
+        elif recv_info['status'] == 'SENT_OK':
+            print 'Message sent!'
+            continue;
+        elif recv_info['status'] == 'OFFLINE':
+            print 'Error: Receiver Offline!'
+            continue;
+        elif recv_info['status'] == 'LOGIN':
+            print recv_info['sourceip']+":"+str(recv_info['sourceport'])+" logged in!"
+            continue;
+        elif recv_info['status'] == 'LOGOUT':
+            print recv_info['sourceip']+":"+str(recv_info['sourceport'])+" logged out!"
+            continue;
+        elif recv_info['status'] == 'DISCONNECT_OK':
+            s.close()
+            print 'Disconnected!'
+            break;
         else:
             print recv_info
             continue;
-
+    return;
 
 def index ():
     s = connect_to_server()
@@ -80,6 +94,9 @@ def index ():
         if (command[0] == 'quit'):
             disconnect(s);
             break;
+        elif (command[0] == 'list'):
+            send_request(s, 'get_user_list')
+            continue;
         elif (command[0] == 'send'):
             try:
                 send_msg(command[1], command[2], s)
--- server/server.py
+++ server/server.py
@@ -2,13 +2,13 @@ import socket
 import sys
 import json
 import threading
-f = open("config.json", 'r')
+f = open("config.json", 'r+b')
 setting = json.load(f)
 host = setting['host']
 port = int(setting['port'])
 cons = []
 addrs = []
-info = {"status": "", "info": "", "targetip": "", "sourceip": ""}
+info = {"status": "", "info": "", "targetip": "", "sourceip": "", "sourceport": "", "body": ""}
 
 def create_socket():
     try:
@@ -32,16 +32,29 @@ def bind_socket():
 
 def send_msg(conn, sourceip, msg):
     new_info = info
-    new_info['status'] = "SEND"
+    new_info['status'] = "RECV"
     new_info['info'] = msg['info']
     new_info['targetip'] = msg['targetip']
     new_info['sourceip'] = sourceip
     in_json = json.dumps(new_info)
     conn.sendall(in_json)
 
+def send_info(conn, res):
+    new_info = info
+    new_info['status'] = "RESP"
+    new_info['body'] = res;
+    in_json = json.dumps(new_info)
+    conn.sendall(in_json)
+
+def reply_disconnect_confirm(conn):
+    new_info = info
+    new_info['status'] = "DISCONNECT_OK"
+    in_json = json.dumps(new_info)
+    conn.sendall(in_json)
+
 def reply_ok(conn):
     new_info = info
-    new_info['status'] = "OK"
+    new_info['status'] = "SENT_OK"
     in_json = json.dumps(new_info)
     conn.sendall(in_json)
 
@@ -51,35 +64,58 @@ def reply_offline(conn):
     in_json = json.dumps(new_info)
     conn.sendall(in_json)
 
+def push_notification(conn, addr, status):
+    new_info = info
+    new_info['status'] = status
+    new_info['sourceip'] = addr[0]
+    new_info['sourceport'] = addr[1]
+    in_json = json.dumps(new_info)
+    conn.sendall(in_json)
+
+
 def handle_connection(conn, addr):
+    for i in range(0, len(cons)):
+        push_notification(cons[i], addr, "LOGIN");
     cons.append(conn)
     addrs.append(addr)
-    print 'connected with ' + addr[0] + ':' + str(addr[1])
+    print addr[0] + ':' + str(addr[1]) + ' connected!'
     while 1:
         recv_info = conn.recv(1024)
         recv_info = json.loads(recv_info)
         if recv_info['status'] == 'SEND':
             flag = 0
+            print 'From '+ addr[0]+ ' to '+ recv_info['targetip']+ ':'
+            print recv_info['info']
             for i in range(0, len(addrs)):
-		print addrs[i][0]
                 if addrs[i][0] == recv_info['targetip']:
                     send_msg(cons[i], addr[0], recv_info);
-                    print 'From ', addr[0], ' to ', recv_info['targetip'], ':'
-                    print recv_info['info']
-                    flag = 1
                     reply_ok(conn);
+                    flag = 1
                     break;
             if flag == 0:
                 reply_offline(conn);
+
+        elif recv_info['status'] == "GET":
+            if recv_info['body'] == "get_user_list":
+                res = []
+                for i in range(0, len(addrs)):
+                    res.append(addrs[i]);
+                send_info(conn, res)
+
         elif recv_info['status'] == 'DISCONNECT':
-            for i in addrs:
+            for i in range(0, len(addrs)):
                 if addrs[i][0] == addr[0]:
-                    addrs.remove(i)
-                    cons.remove(i)
-                    print addr[0], " disconnected"
+                    addrs.remove(addrs[i])
+                    cons.remove(cons[i])
+                    for i in range(0, len(cons)):
+                        push_notification(cons[i], addr, "LOGOUT")
+                    print addr[0]+ ":"+ str(addr[1])+ " disconnected"
+                    reply_disconnect_confirm(conn)
                     break;
             conn.close()
-            break;
+            return;
+
+        continue;
     return;
 
 def index():
