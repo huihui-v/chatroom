fd6ca3234cbc1773076a676b5965518300ea3c8c
Jianfei2333 <xjf999999@hotmail.com>
2017-05-16 11:43:47 +0800

    v2.0 测试版本，添加了服务端与客户端的逻辑功能

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
 import socket
 import sys
 import json
-
+import threading
 f = open("config.json", 'r')
 setting = json.load(f)
 host = setting['host']
 port = int(setting['port'])
+info = {"status": "", "info": "", "targetip": ""}
 
-def connectSocket():
+def create_socket():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error, msg:
@@ -16,15 +17,68 @@ def connectSocket():
     print 'Socket Created'
     return s;
 
-def test():
-    s = connectSocket();
-    string = 'teststring';
+def connect_to_server():
+    s = create_socket();
     try:
         s.connect((host, port))
-        s.send(string)
     except socket.error, msg:
-        print 'FAILED to create socket. Error code: ' + str(msg[0]) + ', Error message: ' + msg[1]
+        print 'FAILED to create connection. Error code: ' + str(msg[0]) + ', Error message: ' + msg[1]
         sys.exit()
-    print 'msg sent'
+    print 'Connection created! Please input your command'
+    return s;
 
-test();
+def input_command ():
+    string = raw_input("input your command: ")
+    return str(string).split(' ');
+
+def disconnect(s):
+    new_info = info;
+    new_info.status = "DISCONNECT"
+    in_json = json.dumps(new_info);
+    s.sendall(in_json)
+    recv_info = s.recv(1024)
+    recv_info = json.loads(recv_info);
+    if recv_info['status'] == 'OK':
+        s.close()
+    print 'Disconnected!'
+
+def send_msg(target, info):
+    new_info = info;
+    new_info.status = "SEND"
+    new_info.targetip = target
+    new_info.info = info
+    in_json = json.dumps(new_info);
+    s.sendall(in_json)
+    recv_info = s.recv(1024)
+    recv_info = json.loads(recv_info);
+    if recv_info['status'] == 'OK':
+        print 'Message sent!'
+    elif recv_info['status'] == 'OFFLINE':
+        print 'Error: Receiver Offline!'
+
+def listen (s):
+    while 1:
+        recv_info = s.recv(1024)
+        recv_info = json.loads(recv_info);
+        if recv_info['status'] == 'SEND':
+            print 'Receive from ', recv_info['sourceip']:
+            print recv_info['info']
+        else:
+            print recv
+
+def index ():
+    s = connect_to_server()
+    thread = threading.Thread(target = listen, args = s)
+    thread.start()
+    while(1):
+        command = input_command()
+        if (command[0] == 'quit'):
+            disconnect(s);
+            break;
+        elif (command[0] == 'send'):
+            try:
+                send_msg(command[1], command[2])
+            except Exception, e:
+                print Exception, ":", e
+
+index();
--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 import socket
 import sys
 import json
-
+import threading
 f = open("config.json", 'r')
 setting = json.load(f)
 host = setting['host']
 port = int(setting['port'])
+cons = []
+addrs = []
+info = {"status": "", "info": "", "targetip": "", "sourceip": ""}
 
-def connectSocket():
+def create_socket():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error, msg:
@@ -16,22 +19,73 @@ def connectSocket():
     print 'Socket Created'
     return s;
 
-def test():
-    s = connectSocket();
-    string = "";
+def bind_socket():
+    s = create_socket();
     try:
         s.bind((host, port))
         s.listen(10)
-        print 'socket now listening'
-        conn, addr = s.accept()
-        print 'connected with ' + addr[0] + ':' + str(addr[1])
-
-        srting = conn.recv(1024)
-        print string;
-        
     except socket.error, msg:
         print 'FAILED to bind port. Error code: ' + str(msg[0]) + ', Error message: ' + msg[1]
         sys.exit()
-    print 'msg received'
+    print 'socket now listening'
+    return s;
 
-test();
+def send_msg(conn, sourceip, msg):
+    new_info = info
+    new_info.status = "SEND"
+    new_info.info = msg['info']
+    new_info.targetip = msg['targetip']
+    new_info.sourceip = sourceip
+    in_json = json.dumps(new_info)
+    conn.sendall(in_json)
+
+def reply_ok(conn):
+    new_info = info
+    new_info.status = "OK"
+    in_json = json.dumps(new_info)
+    conn.sendall(in_json)
+
+def reply_offline(conn):
+    new_info = info
+    new_info.status = "OFFLINE"
+    in_json = json.dumps(new_info)
+    conn.sendall(in_json)
+
+def handle_connection(conn, addr):
+    cons.append(conn)
+    addrs.append(addr)
+    print 'connected with ' + addr[0] + ':' + str(addr[1])
+    while 1:
+        recv_info = conn.recv(1024)
+        recv_info = json.loads(recv_info)
+        if recv_info['status'] = 'SEND':
+            flag = 0
+            for i in addrs:
+                if addrs[i][0] == recv_info['targetip']:
+                    send_msg(cons[i], addr[0], recv_info);
+                    print 'From ', addr[0], ' to ', recv_info['targetip'], ':'
+                    print recv_info['info']
+                    flag = 1
+                    reply_ok(conn);
+                    break;
+            if flag == 0:
+                reply_offline(conn);
+        elif recv_info['status'] = 'DISCONNECT':
+            for i in addrs:
+                if addrs[i][0] == addr[0]:
+                    addrs.remove(i)
+                    cons.remove(i)
+                    print addr[0], " disconnected"
+                    break;
+            conn.close()
+            break;
+    return;
+
+def index():
+    s = bind_socket()
+    while 1:
+        conn, addr = s.accept()
+        thread = threading.Thread(target=handle_connection, args=(conn, addr))
+        thread.start()
+
+index();
